464b4f3228b874a391030a839600afc1dcbaa5a8
Tugume William Mutara <mutara.w@northeastern.edu>
2024-08-27 08:39:17 -0400

    initial commit

--- project247livesports/settings.py
+++ project247livesports/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-2#yt$rc*9(ej3jl&w2e01v4ks9oaxd-j%%m!=qwx4^6k+b+d2u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['www.247livesports.com', '247livesports.com', '127.0.0.1', 'https://d1oxgwekcm5ysg.cloudfront.net']
+ALLOWED_HOSTS = ['www.247livesports.com', '247livesports.com', '127.0.0.1', 'https://d1oxgwekcm5ysg.cloudfront.net', '18.189.13.141']
 
 # Application definition
 
